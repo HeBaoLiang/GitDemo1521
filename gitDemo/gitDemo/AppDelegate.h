e84403840f02cc3d6daae470490025ec2680a85b
hbliang <1026924552@qq.com>
2016-04-02 15:42:23 +0800

    first commit pro

---
+++ gitDemo/gitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitDemo
+//
+//  Created by qianfeng on 16/4/2.
+//  Copyright © 2016年 何宝亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
