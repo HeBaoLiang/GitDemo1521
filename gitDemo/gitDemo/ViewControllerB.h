73365d4b77df0c1d994aee2a65302d8d27c2aa3d
hbliang <1026924552@qq.com>
2016-04-02 16:35:34 +0800

    B commit second

---
+++ gitDemo/gitDemo/ViewControllerB.h
@@ -0,0 +1,13 @@
+//
+//  ViewControllerB.h
+//  gitDemo
+//
+//  Created by qianfeng on 16/4/2.
+//  Copyright © 2016年 何宝亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewControllerB : UIViewController
+
+@end
